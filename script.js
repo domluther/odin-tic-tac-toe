9522b48d48e7cea4c476cfe9576426d842091577
dom <35171942+domluther@users.noreply.github.com>
2023-05-07 16:07:13 +0100

    AI mode - imperfect but better than random - code got ugly

--- script.js
+++ script.js
@@ -54,8 +54,8 @@ const gameBoard = (() => {
 
   // If the place is taken, reject the move - also works if out of bound as that is undefined
   const addPiece = (x, y, mark) => {
+    // space taken
     if (gameboard[x][y] !== ' ') {
-      console.log('Space taken');
       return false;
     }
 
@@ -65,16 +65,19 @@ const gameBoard = (() => {
   };
 
   // Check an individual line for a win
-  const checkLine = (line, mark) => {
+  const checkLine = (line, mark, count = 3) => {
     const res = line
       .map((coords) => gameboard[coords[0]][coords[1]] === mark)
       .reduce((acc, cur) => (cur === true ? acc + 1 : acc), 0);
 
-    return res === 3;
+    // if (res === 2) {
+    //   console.log(line);
+    // }
+    return res === count;
   };
 
   // Are there any wins?
-  const checkBoardForWin = (mark) => {
+  const checkBoardForWin = (mark, count = 3) => {
     // The 8 winning lines in noughts and crosses
     const winningLines = [
       [
@@ -118,20 +121,53 @@ const gameBoard = (() => {
         [2, 2],
       ],
     ];
+    // Is it a win?
+    if (count === 3) {
+      const results = winningLines
+        // Check each of the possible winning line combinations for a win
+        .map((line) => checkLine(line, mark, count))
+        // just keep the trues
+        .filter((value) => value === true);
+
+      // Are there any trues?
+      return results.length > 0;
+    }
+    // When looking for potential wins
 
     const results = winningLines
-      // Check each of the possible winning line combinations for a win
-      .map((line) => checkLine(line, mark))
-      // just keep the trues
-      .filter((value) => value === true);
+      // Check each of the line combinations for a possible win (2 marks of the correct type)
+      .map((line) => checkLine(line, mark, count))
+      .reduce((arr, ele, index) => {
+        if (ele === true) arr.push(index);
+        return arr;
+      }, []);
+    // Is there a true? If so which line
+    let lineOfInterest;
+    results.forEach((index) => {
+      if (checkLine(winningLines[index], ' ', 1)) {
+        lineOfInterest = winningLines[index];
+      }
+    });
+    if (lineOfInterest) return lineOfInterest;
+    return false;
+  };
 
-    // Are there any trues?
-    return results.length > 0;
+  const checkBoardForPotentialWin = (mark) => {
+    const lineOfInterest = checkBoardForWin(mark, 2);
+    console.log(lineOfInterest);
+    return lineOfInterest;
   };
 
   // Init the board
   createBoard();
-  return { getBoard, addPiece, checkBoardForWin, renderBoard, resetBoard };
+  return {
+    getBoard,
+    addPiece,
+    checkBoardForWin,
+    renderBoard,
+    resetBoard,
+    checkBoardForPotentialWin,
+  };
 })();
 
 const Player = (name, desiredMark) => {
@@ -141,6 +177,8 @@ const Player = (name, desiredMark) => {
 };
 
 let players = [];
+let mode;
+let aiMovesMade = [];
 
 const gameController = (() => {
   // State variables needed for the game
@@ -151,9 +189,116 @@ const gameController = (() => {
   let playerPointer;
   let won;
 
+  const corners = [
+    [0, 0],
+    [0, 2],
+    [2, 2],
+    [2, 0],
+  ];
+
   const updateStatus = (message) => {
     const gameStatusEle = document.querySelector('.game-status');
     gameStatusEle.textContent = message;
+  };
+
+  const randomFromList = (items) =>
+    items[Math.floor(Math.random() * items.length)];
+
+  const randomMove = () => {
+    let validMove = false;
+    while (!validMove) {
+      const move = [
+        Math.floor(Math.random() * 3),
+        Math.floor(Math.random() * 3),
+      ];
+      console.log(`Randomly trying ${move}`);
+      validMove = gameBoard.addPiece(move[0], move[1], activePlayer.mark);
+      if (validMove) {
+        aiMovesMade.push(corners.findIndex((corner) => corner === move));
+      }
+    }
+  };
+
+  const randomCorner = () => {
+    let validMove = false;
+    while (!validMove) {
+      const move = randomFromList(corners);
+      validMove = gameBoard.addPiece(move[0], move[1], activePlayer.mark);
+      if (validMove) {
+        aiMovesMade.push(corners.findIndex((corner) => corner === move));
+      }
+    }
+  };
+
+  const fromLineOfInterest = (lineOfInterest) => {
+    let validMove = false;
+    while (!validMove) {
+      const move = randomFromList(lineOfInterest);
+      validMove = gameBoard.addPiece(move[0], move[1], activePlayer.mark);
+    }
+    console.log('Interesting line!');
+  };
+
+  const aiMove = () => {
+    if (mode === 'two-player') throw Error('In wrong mode');
+
+    // Move 1: Go in a corner.
+    if (moves === 1) {
+      randomCorner();
+    }
+    // Move 2: If the other player did not go there then go in the opposite corner to move 1. Otherwise go in a free corner.
+    else if (moves === 3) {
+      // Find the co-ordinates of the opposite corner
+      const move = corners.at(aiMovesMade[0] - 2);
+      const validMove = gameBoard.addPiece(move[0], move[1], activePlayer.mark);
+      if (!validMove) {
+        randomCorner();
+      }
+    }
+    // Move 3: If there are two Xs and a space in a line (in any order) then go in that space. Otherwise if there are two Os and a space in a line then go in that space. Otherwise go in a free corner.
+    // Move 4: If there are two Xs and a space in a line (in any order) then go in that space. Otherwise if there are two Os and a space in a line then go in that space. Otherwise go in a free corner.
+    else if (moves === 5 || moves === 7) {
+      // Can I win?
+      const lineOfInterestMe = gameBoard.checkBoardForPotentialWin(
+        activePlayer.mark
+      );
+      if (lineOfInterestMe) {
+        fromLineOfInterest(lineOfInterestMe);
+      } else {
+        // Can I block you?
+        const lineOfInterestYou = gameBoard.checkBoardForPotentialWin('O');
+        if (lineOfInterestYou) {
+          fromLineOfInterest(lineOfInterestYou);
+        } else {
+          randomCorner();
+        }
+      }
+    } else {
+      // Move 5: Go in the free space.
+      randomMove();
+    }
+    console.log(`${moves} ${gameBoard.getBoard()}`);
+    gameBoard.renderBoard();
+    won = gameBoard.checkBoardForWin(activePlayer.mark);
+    // Not won? Swap the player
+    if (!won) {
+      activePlayerId = activePlayerId === 1 ? 0 : 1;
+      activePlayer = players[activePlayerId];
+      playerPointer = activePlayerId === 0 ? '<<' : '>>';
+      moves += 1;
+      // Is it game over? (board full)
+      if (moves < maxMoves) {
+        updateStatus(
+          `${players[0].playerName} ${playerPointer} ${players[1].playerName}`
+        );
+      } else {
+        updateStatus('A draw');
+      }
+    }
+    if (won) {
+      updateStatus(`${activePlayer.playerName} wins!`);
+      gameContentEle.removeEventListener('click', tryToMove);
+    }
   };
 
   const tryToMove = (e) => {
@@ -172,13 +317,15 @@ const gameController = (() => {
         activePlayerId = activePlayerId === 1 ? 0 : 1;
         activePlayer = players[activePlayerId];
         playerPointer = activePlayerId === 0 ? '<<' : '>>';
-
         moves += 1;
         // Is it game over? (board full)
         if (moves < maxMoves) {
           updateStatus(
             `${players[0].playerName} ${playerPointer} ${players[1].playerName}`
           );
+          if (mode === 'v-computer') {
+            aiMove();
+          }
         } else {
           updateStatus('A draw');
         }
@@ -215,6 +362,7 @@ restartBtn.addEventListener('click', () => {
   document.querySelector('.game-controls').classList.add('hidden');
   document.querySelector('.game-settings').classList.remove('hidden');
 });
+
 settingsForm.addEventListener('submit', (e) => {
   e.preventDefault();
   // Toggle visibility of elements
@@ -226,8 +374,10 @@ settingsForm.addEventListener('submit', (e) => {
   // Read in names
   const player1Name = document.querySelector('#player1Name').value;
   const player2Name = document.querySelector('#player2Name').value;
+  mode = document.querySelector('#game-mode').value;
 
   // Create player objects
   players = [Player(player1Name, 'O'), Player(player2Name, 'X')];
+  aiMovesMade = [];
   init();
 });
